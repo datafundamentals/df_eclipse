3632a2dfcae836b7d1b8ef1838deb1377bbc0b03
jcarapet <jeffcarapetyan@gmail.com>
2014-05-30 13:40:45 -0500

    adjusted following manual style on the web

--- Berksfile
+++ Berksfile
@@ -1,3 +1,4 @@
 source "https://api.berkshelf.com"
 
 metadata
+cookbook "df_java" path: "/Users/thisguy/work/cookbook/df_java"
--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,45 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+include_recipe "libarchive"
+include_recipe "df_java::default"
+
+directory "/usr/local/eclipse" do 
+	recursive true
+	mode "0777"
+	action :create 
+end
+
+libarchive_file "eclipse-standard-kepler-SR2-linux-gtk-x86_64.tar.gz" do 
+	path "/vagrant/binaries/eclipse-standard-kepler-SR2-linux-gtk-x86_64.tar.gz"
+	extract_to "/opt"
+	owner "vagrant"
+	group "vagrant" 
+	action :extract 
+end 
+
+file "eclipse.desktop" do 
+	cwd "/home/vagran/desktop" 
+	mode "0777" 
+	content "[Desktop Entry]\n
+Name=Eclipse\n 
+Type=Application\n 
+Exec=eclipse\n 
+Terminal=false\n 
+Icon=eclipse\n 
+Comment=Integrated Development Environment\n 
+NoDisplay=false\n
+Categories=Development;IDE;\n
+Name[en]=Eclipse"
+action :create
+end
+
+execute "desktop-file-install eclipse.desktop" do 
+	action :run 
+end
+ 
+ link "/usr/local/bin" do 
+ 	to "/opt/eclipse/eclipse" 
+ 	link_type :symbolic
+ end
