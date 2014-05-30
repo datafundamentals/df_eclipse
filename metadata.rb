53b30e10c4346e31341a65ee8cf9d66ca0855ed2
jcarapet <jeffcarapetyan@gmail.com>
2014-05-30 14:08:10 -0500

    added metadata dependency

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,8 @@ maintainer_email 'YOUR_EMAIL'
 license          'All rights reserved'
 description      'Installs/Configures df_eclipse'
 long_description 'Installs/Configures df_eclipse'
-version          '0.1.2'
+version          '0.1.3'
+
+depends "df_java"
+depends 
 
