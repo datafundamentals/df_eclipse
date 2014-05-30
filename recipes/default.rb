0a73a359072be3f918ff6263297706ef28f2f3b6
jcarapet <jeffcarapetyan@gmail.com>
2014-05-30 14:05:27 -0500

    rewrote so no libarchive

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,25 +7,17 @@
 # All rights reserved - Do Not Redistribute
 #
 
-include_recipe "libarchive::default" 
 include_recipe "df_java::default"
 
-directory "/usr/local/eclipse" do 
-	recursive true
-	mode "0777"
-	action :create 
-end
 
-libarchive_file "eclipse-standard-kepler-SR2-linux-gtk-x86_64.tar.gz" do 
-	path "/vagrant/binaries/eclipse-standard-kepler-SR2-linux-gtk-x86_64.tar.gz"
-	extract_to "/opt"
-	owner "vagrant"
-	group "vagrant" 
-	action :extract 
-end 
+execute "tar -xzvf /vagrant/binaries/eclipse-standard-kepler-SR2-linux-gtk-x86_64.tar.gz -C /opt" do
+	user "root"
+	action :run
+	not_if do FileTest.directory?("/opt/eclipse/eclipse")end
+	end
 
 file "eclipse.desktop" do 
-	cwd "/home/vagran/desktop" 
+	cwd "/home/vagrant/desktop" 
 	mode "0777" 
 	content "[Desktop Entry]\n
 Name=Eclipse\n 
