545fc00677f7bea4f768ce6e8b3815778922a693
jcarapet <jeffcarapetyan@gmail.com>
2014-06-02 12:24:37 -0500

    an actual working version.

--- recipes/default.rb
+++ recipes/default.rb
@@ -12,30 +12,16 @@
 execute "tar -xzvf /vagrant/binaries/eclipse-standard-kepler-SR2-linux-gtk-x86_64.tar.gz -C /opt" do
 	user "root"
 	action :run
-	not_if do FileTest.directory?("/opt/eclipse/eclipse")end
 	end
 
-file "eclipse.desktop" do 
-	cwd "/home/vagrant/desktop" 
+file "/home/vagrant/Desktop/eclipse.desktop" do  
 	mode "0777" 
-	content "[Desktop Entry]\n
-Name=Eclipse\n 
-Type=Application\n 
-Exec=eclipse\n 
-Terminal=false\n 
-Icon=eclipse\n 
-Comment=Integrated Development Environment\n 
-NoDisplay=false\n
-Categories=Development;IDE;\n
-Name[en]=Eclipse"
+	content "[Desktop Entry]\nName=Eclipse\nType=Application\nExec=/opt/eclipse/eclipse\nTerminal=false\nIcon=/opt/eclipse/icon.xmp\nComment=Integrated Development Environment\nNoDisplay=false\nCategories=Development;IDE;\nName[en]=Eclipse"
 action :create
 end
 
 execute "desktop-file-install eclipse.desktop" do 
+	cwd "/home/vagrant/Desktop"
 	action :run 
 end
- 
- link "/usr/local/bin" do 
- 	to "/opt/eclipse/eclipse" 
- 	link_type :symbolic
- end
+
